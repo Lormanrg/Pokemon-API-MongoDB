c34c59f2bf6c0fc3d9e838dd7b6d1ebb22f48fb7
Lorman <lormanrg@gmail.com>
2024-09-03 23:08:47 -0400

    Entity properties have been implemented

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,19 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Mongoose } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Mongoose {
+  // Mongo ID me lo da Mongo
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
