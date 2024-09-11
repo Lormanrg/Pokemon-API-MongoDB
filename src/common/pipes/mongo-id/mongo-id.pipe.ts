0fde711633520bc9cc600cd2e18f9de3b713114a
Lorman <lormanrg@gmail.com>
2024-09-11 12:01:25 -0400

    ParseMongoIdPipe validation applied

--- src/common/pipes/mongo-id/mongo-id.pipe.ts
+++ src/common/pipes/mongo-id/mongo-id.pipe.ts
@@ -7,7 +7,7 @@ import {
 import { isValidObjectId } from 'mongoose';
 
 @Injectable()
-export class MongoIdPipe implements PipeTransform {
+export class ParseMongoIdPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
     if (!isValidObjectId(value)) {
       throw new BadRequestException(`${value} is not a valid MongoId`);
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -81,6 +81,7 @@ export class PokemonService {
 
     // await pokemon.deleteOne();
     // const result = await this.pokemonModel.findByIdAndDelete(id);
+
     const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
     if (deletedCount === 0) {
       throw new BadRequestException(`MongoId with ${id} not found`);
