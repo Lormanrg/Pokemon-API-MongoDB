56f012de0be7d188df7eaa8ac59d9b0ddded1929
Lorman <lormanrg@gmail.com>
2024-09-11 17:16:51 -0400

    Seed to save Pokemons from PokeAPi created successfully

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
@@ -14,9 +21,10 @@ export class SeedService {
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      console.log({ name, no });
+
+      const createPokemons = this.pokemonModel.create({ name, no });
     });
 
-    return data.results;
+    return 'Seed Executed';
   }
 }
